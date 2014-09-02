97c9884d157519dcb84371d75de11fe004599b62
drmolloy <drmolloy@student.naperville203.org>
2014-09-02 15:08:42 -0500

    drmolloy
    exercises 2.4 and 2.9

---
+++ exercise 1.15/AddTester.java
@@ -0,0 +1,12 @@
+import java.awt.Rectangle;
+public class AddTester
+{
+   public static void main(String[] args)
+   {
+       Rectangle r1 = new Rectangle(0, 0, 100, 50);
+       r1.add(0,0);
+       System.out.println(r1);
+    }
+}
+       
+   
